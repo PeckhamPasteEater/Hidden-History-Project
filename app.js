255799bf85709d6e46a43e8c5d1fe6fd05d7433e
PeckhamPasteEater <27rchen@pinewood.edu>
2026-01-11 15:54:46 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ let notified = new Set();
 
 const map = L.map("map").setView([locations[0].lat, locations[0].lng], 15);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "© OpenStreetMap contributors"
-}).addTo(map);
+L.tileLayer(
+  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    maxZoom: 19,
+    attribution: "© OpenStreetMap contributors",
+    crossOrigin: true
+  }
+).addTo(map);
+
 
 const markers = {};
 
