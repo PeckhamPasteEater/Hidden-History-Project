c5d2dbe30d7c867d7866c52d61311ffbcab18ce3
PeckhamPasteEater <27rchen@pinewood.edu>
2026-03-03 17:40:55 -0800

    Update locations.js

--- locations.js
+++ locations.js
@@ -4,7 +4,7 @@ const locations = [
     title: "Duvencks Part 1: The Duvenecks Arrive in the Bay Area",
     lat: 37.44472,
     lng: -122.15298,
-    category: "duvenecks",
+    category: ["pre_silicon_valley", "duvenecks"],
     description: "Part 1 of a series of sites celebrating the founders of Hidden Villa, the Duvenecks. The Duvenecks, born in to East Coast privilege, had visited, and, as such, been enchanted by California in their travels around the world. However, it wasn't until a particularly harsh winter in their native Boston that they decided to officially light out for the West. As World War I raged, they settled in temperate Santa Barbara for a short period, only to move to Carmel and later Tacoma, Washington on account of Frank's enlisting as an engineer in the military. Soon, Frank was sent to the European theatre of war, but not before ensuring Josephine learned how to drive a Ford Model T. Eight months pregnant with her third child, Josephine brought the family to Palo Alto in 1918, then and still a university town, albeit then one of under 5,000 residents. Josephine rented a house here, at the crook of Addison and Cowper.",
     image: ""
   },
@@ -13,7 +13,7 @@ const locations = [
     title: "Duvenecks Part 2: Frank and Josephine Return",
     lat: 37.44855,
     lng: -122.14005,
-    category: "duvenecks",
+    category: ["pre_silicon_valley", "duvenecks"],
     description: "Part 2 of a series of sites celebrating the founders of Hidden Villa, the Duvenecks. Read preceding sites for complete narrative. Frank was only away for one year. Much had changed; after giving birth to Hope in Palo Alto, Josephine had also gone away to care for Frank's father in Cincinnati, Ohio, who had developed cancer due to a lifetime of smoking. When Josephine returned, she bought a house here on Newell Road for a sum total of $7,500. What is now South Bay suburban sprawl was once the city's limit. The Duvenecks, standing here, would be perched on the edge of human settlement. The year was 1919; as a member of the occupying force of Germany, Frank remained after the armistice until August. When he returned, he saw his daughter for the first time.",
     image: ""
   },
@@ -112,7 +112,7 @@ const locations = [
     title: "O'Keefe Ohlone Settlement",
     lat: 37.36523,
     lng: -122.11725,
-    category: ["ohlone", "duvenecks"],
+    category: "ohlone",
     description: "Placeholder. In collaboration with Hidden Villa",
     image: ""
   },
@@ -121,7 +121,7 @@ const locations = [
     title: "Russian Ridge Bedrocks",
     lat: 37.3153,
     lng: -122.1882,
-    category: ["ohlone", "duvenecks"],
+    category: "ohlone",
     description: "Placeholder. In collaboration with Hidden Villa",
     image: ""
   },
@@ -130,7 +130,7 @@ const locations = [
     title: "Duvenecks Part 3: The Duveneck House",
     lat: 37.35011,
     lng: -122.16298,
-    category: "duvenecks",
+    category: ["pre_silicon_valley", "duvenecks"],
     description: "Part 3 in a series of sites celebrating the founders of Hidden Villa, the Duvenecks. Read preceding sites for complete narrative. The Duvenecks spent any spare time that they had outdoors. In their Ford Model T, they would cruise through the then-treacherous Santa Cruz mountains, camping on hillsides and in clearings. They had always dreamed of owning their own open space. In 1924, they found it on a routine drive down Moody Road. Spying the familiar metal flags as well as a 'for sale' sign, they were intrigued: they turned in, and discovered the thousand-acre lot that would serve both as their home and a setting for some of the Bay Area's most significant history: Hidden Villa. Amazed by its pristine, unblemished nature, the Duvenecks purchased the lot. They completed the Duveneck House in 1929, completed in a Spanish colonial style, and laden with intricate ironwork. In collaboration with Hidden Villa",
     image: ""
   },
@@ -139,7 +139,7 @@ const locations = [
     title: "Hidden Villa Hostel",
     lat: 37.35055,
     lng: -122.16010,
-    category: "duvenecks",
+    category: ["pre_silicon_valley", "duvenecks"],
     description: "Placeholder. In collaboration with Hidden Villa",
     image: ""
   },
